95752af0d0d20770c58d8320cda304d78cd22f48
Juan Esteban Castaño Osma <osmajuan760@gmail.com>
2023-01-07 03:33:52 -0500

    terminada toda la logica del programa, solo falta la funcionalidad del boton de copiar

---
+++ code/index.js
@@ -0,0 +1,102 @@
+const encryptButton = document.querySelector(".encrypt");
+const decryptButton = document.querySelector(".decrypt");
+
+const inputArea = document.querySelector(".input");
+const resultArea = document.querySelector(".result");
+
+const messageNotFound = `<img src="../resources/Muñeco.png" alt="Imagen que representa que ningun mensaje ha sido procesado"
+class="messageNotFound">
+<h2>Ningún mensaje fue encontrado</h2>
+<p>Ingresa el texto que desees encriptar o desencriptar</p>`
+
+const htmlButton = "<button class='btnCopy'>Copiar</button>";
+
+const conversion = {
+
+    "a": "ai",
+    "e": "enter",
+    "i": "imes",
+    "o": "ober",
+    "u": "ufat"
+
+}
+
+const vocales = Object.keys(conversion);
+
+encryptButton.onclick = () => {
+
+    let texto = inputArea.value;
+    if (validarTexto(texto)) {
+
+        resultArea.innerHTML = `<textarea class='textResult'>${encriptarTexto(texto)}</textarea>${htmlButton}`;
+        resultArea.style.justifyContent = "space-between";
+
+    } else {
+
+        resultArea.innerHTML = messageNotFound;
+
+    }
+
+}
+
+decryptButton.onclick = () => {
+
+    let texto = inputArea.value;
+    if (validarTexto(texto)) {
+
+        resultArea.innerHTML = `<textarea>${desencriptarTexto(texto)}</textarea>${htmlButton}`;
+
+    } else {
+
+        resultArea.innerHTML = messageNotFound;
+
+    }
+
+}
+
+function validarTexto(texto) {
+
+    if (texto !== "") {
+        return true;
+    }
+
+    return false;
+}
+
+function encriptarTexto(texto) {
+
+    let auxText = "";
+    for (let i = 0; i < texto.length; i++) {
+
+        let element = texto[i];
+        if (Object.keys(conversion).indexOf(element) !== -1) {
+
+            auxText += conversion[element];
+
+        } else {
+
+            auxText += element;
+
+        }
+    }
+
+    return auxText;
+}
+
+function desencriptarTexto(texto) {
+
+    let auxText = "";
+    Object.values(conversion).forEach((value, index, array) => {
+
+        auxText = texto.replace(new RegExp(value, "g"), getKeyByValue(conversion, value));
+        texto = auxText;
+
+    })
+
+    return texto;
+
+}
+
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+}
